2db025a0578785957b3e61790efd6e9d94c82876
Ryan Eastridge <ryan@eastridge.me>
2013-03-10 17:56:15 -0700

    JS logo

--- index.js
+++ index.js
@@ -41,54 +41,36 @@ function initBoard(board) {
     response.send();
   });
 
-  var color = 254;
-  var frame = _.range(0, 64).map(function(i) {
-    if (i < 16) {
-      return [color, color, 0];
-    } else if (i < 32) {
-      return [color, 0, color];
-    } else if (i < 48) {
-      return [0, color, color];
-    } else {
-      return [color, color, color];
-    }
-  });
-  board.writeFrame(frame, function(){
-    board.clear(function() {
-      //setInterval(function() {
-      //  board.fadePixel(Math.floor(Math.random() * 64), [0, 0, 0], [254, 254, 254], 2000);
-      //}, 33);
-    });
+  startup(board);
+}
+
+function startup(board, next) {
+  var yellw = [241, 220, 63],
+    black = [50, 51, 48];
+
+  var jsLogo = [
+    yellw, yellw, yellw, yellw, yellw, yellw, yellw, yellw,
+    yellw, yellw, yellw, yellw, yellw, yellw, yellw, yellw,
+    yellw, yellw, yellw, yellw, yellw, yellw, yellw, yellw,
+    yellw, black, black, black, yellw, black, black, black,
+    yellw, yellw, black, yellw, yellw, black, yellw, yellw,
+    yellw, yellw, black, yellw, yellw, black, black, black,
+    yellw, yellw, black, yellw, yellw, yellw, yellw, black,
+    yellw, black, black, yellw, yellw, black, black, black
+  ];
+
+  board.writeFrame(jsLogo, function(){
+    setTimeout(function() {
+      board.clear();
+      if (next) {
+        next();
+      }
+    }, 7000);
   });
 }
 
 server.use(express.bodyParser());
 server.use(express.static('public'));
 server.listen(8015);
-
-
-/*
-
-
-port.on("open", function () {
-  console.log('open');
-  port.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-  port.write([255, 2, 0, 50, 254, 254, 254], function(err, results) {
-    console.log(err);
-    console.log(results);
-  });
-});
-*/
-
-/*
-
-  [255, 10] = kill the board
-  [255, 20] = test sequence
-  [255, 1, 0, pixelNum, r, g, b, r, g, r, g, b ...] (rgb * 16), set 16 pixels
-  [255, 2, 0, pixelNum, r, g, b] = set a pixel
-
-*/
 
 
